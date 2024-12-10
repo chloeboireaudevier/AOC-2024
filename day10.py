f4406d7683804102b4518f761fee2dec6caaa6a1
chloeboireaudevier <cbd65@hotmail.fr>
2024-12-10 11:51:37 +0100

    day10, part2

--- day10.py
+++ day10.py
@@ -36,7 +36,18 @@ def count_trailhead_score(data,i,j,n,m,nines): #rec ?
             if 0<=i+move[0]<n and 0<= j+move[1] < m and data[i+move[0]][j+move[1]] == str(int(data[i][j])+1):
                 count += count_trailhead_score(data,i+move[0],j+move[1],n,m,nines)
         return count
-                
+    
+def count_trailhead_score_rating(data,i,j,n,m):
+    #print(data[i][j])
+    if data[i][j] == '9':
+        return 1
+    else:
+        moves = [(-1,0),(1,0),(0,1),(0,-1)]
+        count = 0
+        for move in moves:
+            if 0<=i+move[0]<n and 0<= j+move[1] < m and data[i+move[0]][j+move[1]] == str(int(data[i][j])+1):
+                count += count_trailhead_score_rating(data,i+move[0],j+move[1],n,m)
+        return count
 
 def get_scores(data):
     scores = []
@@ -51,4 +62,17 @@ def get_scores(data):
     #print(coord_zeros)
     return scores,sum(scores)
 
-print(get_scores(content))
+def get_scores_rating(data):
+    scores = []
+    coord_zeros = find_starts(data)
+    n = len(data)
+    m = len(data[0])
+
+    for coord in coord_zeros:
+        score = count_trailhead_score_rating(data,coord[0],coord[1],n,m)
+        scores.append(score)
+    #print(coord_zeros)
+    return scores,sum(scores)
+
+print('Part 1 : ',get_scores(content))
+print('Part 2 : ',get_scores_rating(content))
