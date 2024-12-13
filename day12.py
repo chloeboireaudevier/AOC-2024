17724c4f079ae9c8798b5f9995987465b2bb3571
chloeboireaudevier <cbd65@hotmail.fr>
2024-12-13 23:17:39 +0100

    progress day 12 part2

--- day12.py
+++ day12.py
@@ -1,5 +1,5 @@
-#file = open("testinput/testinputday12-3.txt",'r')
-file = open("input/inputday12.txt",'r')
+file = open("testinput/testinputday12-1.txt",'r')
+#file = open("input/inputday12.txt",'r')
 content = file.readlines()
 file.close()
 #print(content)
@@ -16,13 +16,19 @@ def print_tab(data):
 print_tab(content)
 
 global directions
-directions = [(-1,0),(1,0),(0,1),(0,-1)]
+directions = [(0,1),(-1,0),(0,-1),(1,0)]
 
 def first_occ(data):
     for i in range(len(data)):
         for j in range(len(data[0])):
             if data[i][j] !='.':
                 return data[i][j]
+            
+def first_occ_index(data):
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            if data[i][j] !='.':
+                return i,j
 
 def get_region_from(data,x,y):
     test = [['.' for i in range(len(data[0]))] for i in range(len(data))]
@@ -38,14 +44,6 @@ def get_region_from(data,x,y):
             if 0<=curr[0]+dir[0]<len(data) and 0<=curr[1]+dir[1]<len(data[0]) and data[curr[0]+dir[0]][curr[1]+dir[1]] == car and not (curr[0]+dir[0],curr[1]+dir[1]) in visited and not (curr[0]+dir[0],curr[1]+dir[1]) in stack:
                 stack.append((curr[0]+dir[0],curr[1]+dir[1]))
     return test
-
-
-def transform_into_new_regions(data):
-    pass
-
-
-
-
 
 def get_diff_regions(data):
     names=[]
@@ -91,9 +89,8 @@ def get_regions_area_dict(data,n,m):
                 area[data[i][j]] +=1
     return area
 
-def get_regions_area(data,n,m):
+def get_regions_area(data,n,m,regions):
     area = []
-    regions = get_all_reg(data,n,m)
     for region in regions:
         count = 0
         car = first_occ(region)
@@ -104,9 +101,8 @@ def get_regions_area(data,n,m):
         area.append(count)
     return area
 
-def get_regions_permieter(data,n,m):
+def get_regions_permieter(data,n,m,regions):
     perimeter = []
-    regions = get_all_reg(data,n,m)
     for k in range (len(regions)):
         name = first_occ(regions[k])
         region = regions[k]
@@ -120,16 +116,39 @@ def get_regions_permieter(data,n,m):
         perimeter.append(single_perim)
     return perimeter
 
+def get_regions_side(data,n,m,regions):
+    sides = []
+    for k in range (len(regions)):
+        region = regions[k]
+        start = first_occ_index(region)
+        single_sides = 0
+
+        cur = start
+        curdir = 0
+        while not (data[cur[0]+directions[curdir][0]][cur[1]+directions[curdir][1]] !='.' and data[cur[0]+directions[(curdir+1)%4][0]][cur[1]+directions[(curdir+1)%4][1]] == '.'):
+            curdir = (curdir+1)%4
+        while cur != start:
+            
+            while data[cur[0]+directions[curdir][0]][cur[1]+directions[curdir][1]] !='.' and data[cur[0]+directions[(curdir+1)%4][0]][cur[1]+directions[(curdir+1)%4][1]] == '.':
+                cur = ((cur[0]+directions[curdir][0]),(cur[1]+directions[curdir][1]))
+            curdir(curdir+1)%4
+            single_sides +=1
+
+        sides.append(single_sides)
+    return sides
 
 def get_price_fences(data):
     n = len(data)
     m = len(data[0])
-    area = get_regions_area(data,n,m)
-    perimeter = get_regions_permieter(data,n,m)
+    regions = get_all_reg(data,n,m)
+    area = get_regions_area(data,n,m,regions)
+    perimeter = get_regions_permieter(data,n,m,regions)
     price = 0
     print(area,perimeter)
     for i in range(len(perimeter)):
         price += area[i]*perimeter[i]
     return price
+
+print(get_regions_side(content,len(content),len(content[0]),get_all_reg(content,len(content),len(content[0]))))
 
 print(get_price_fences(content))
