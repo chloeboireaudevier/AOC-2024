cec741bd50f380e36ec6270e644c0a1a8d8a27bf
chloeboireaudevier <cbd65@hotmail.fr>
2024-12-16 13:54:20 +0100

    day 13 part 2 -- pb in solving with negative scalars

--- day13.py
+++ day13.py
@@ -1,5 +1,6 @@
 import math
 import re
+import numpy as np
 
 #file = open("testinput/testinputday13.txt",'r')
 file = open("input/inputday13.txt",'r')
@@ -12,7 +13,14 @@ data = []
 for i in range(0,len(content),4):
     machine = []
     for j in range(3):
-        machine.append([int(s) for s in re.findall(r'\b\d+\b', content[i+j])])
+        #if j < 2:
+            machine.append([int(s) for s in re.findall(r'\b\d+\b', content[i+j])])
+        #PART TWO
+        #else:
+        #    to_app = [int(s) for s in re.findall(r'\b\d+\b', content[i+j])]
+        #    for k in range (len(to_app)):
+        #        to_app[k] += 10000000000000
+        #    machine.append(to_app)
     data.append(machine)
 print(data)
 
@@ -25,24 +33,50 @@ def is_winnable(array):
     mintokens = math.inf
     for i in range(100):
         current = (buttonA[0]*i,buttonA[1]*i)
+
         for j in range(100):
             current = (buttonA[0]*i+j*buttonB[0],buttonA[1]*i+j*buttonB[1])
             if current[0] == prize[0] and current[1] == prize[1]:
                 tokens = i*3 + j
                 if tokens < mintokens:
                     mintokens = tokens
+                    break
                     #print(i,j)
             elif current[0] > prize[0] or current[1] > prize[1]:
                 break
+        if mintokens < math.inf:
+            break
+
     return mintokens if mintokens != math.inf else 0
+
+def is_winnable_part_two(array):
+    buttonA = array[0]
+    buttonB = array[1]
+    prize = array[2]
+
+    x = np.array([[buttonA[0],buttonB[0]],
+                  [buttonA[1],buttonB[1]]])
+    
+    scalars = np.linalg.solve(x,prize)
+    print(scalars)
+    nbA = int(scalars[0])
+    nbB = int(scalars[1])
+
+    print(nbA,nbB)
+
+    if nbA <= 100 and nbB <= 100 and nbA*buttonA[0]+nbB*buttonB[0] == prize[0] and nbA*buttonA[1]+nbB*buttonB[1] == prize[1] :
+        nbTokens = 3*nbA + nbB
+    else:
+        nbTokens = 0
+    return nbTokens
 
 
 def get_prizes(data):
     total_tokens = 0
     for tab in data:
-        total_tokens += is_winnable(tab)
+        total_tokens += is_winnable_part_two(tab)
     return total_tokens
 
-#print(is_winnable(data[2]))
+#print(is_winnable_part_two(data[0]))
 #print(data)
 print(get_prizes(data))
---
+++ tests.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+x = np.array([[94,22], #EN COLONNE
+             [34,67]])
+y = ([8400,5400])
+scalars = np.linalg.solve(x,y)
+print(scalars)
+
+array = [[94,34],[22,67]]
+
+x2 = np.array(array)
+y2 = ([8400,5400])
+print(np.linalg.solve(x2,y2))
+
+print(68.32//10)
