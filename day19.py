ae0c22944c4e7e70c4c853a3efd51a2f0382add4
chloeboireaudevier <cbd65@hotmail.fr>
2024-12-19 07:58:11 +0100

    day 19 part2

--- day19.py
+++ day19.py
@@ -7,7 +7,10 @@ file = open("input/inputday19.txt",'r')
 content = file.readlines()
 file.close()
 
-print(content)
+#print(content)
+
+global pattern_dict
+pattern_dict = {}
 
 def get_index_separator(data):
     index = 0
@@ -20,7 +23,7 @@ def get_input(data):
     index = get_index_separator(data)
     designs = data[:index]
     patterns = data[index+1:]
-    print(designs)
+    #print(designs)
 
     designs = designs[0].split(',')
     designs[-1] = designs[-1][:-1]
@@ -38,12 +41,14 @@ def get_input(data):
 def starts_with(pattern,design):
     starts = True
     i = 0
+    if len(pattern) > len(design):
+        return False
     while starts and i < len(pattern):
+        #print(design, pattern,i)
         if i >= len(design) or pattern[i] != design[i]:
             starts = False
         i+=1
     return starts
-
 
 def get_possible_patterns(patterns,design):
     poss = []
@@ -61,15 +66,33 @@ def is_possible(patterns,design):
         for p in possible_des:
             res = res or is_possible(patterns,p)
         return res
+    
+def get_nb_possible(patterns,design):
+    if design =='':
+        return 1
+    else: #case false traité à l'intérieur ?
+        possible_des = get_possible_patterns(patterns,design)
+        res = 0
+        for p in possible_des:
+            if (design,p) in pattern_dict:
+                res += pattern_dict[design,p]
+            else:
+                pattern_dict[design,p] = get_nb_possible(patterns,p)
+                res += get_nb_possible(patterns,p)
+        return res
 
 def solve(content):
     patterns,designs = get_input(content)
     nbPossible = 0
+    nbArrangements = 0
     for d in designs:
+        #print(d)
         if is_possible(patterns,d):
             nbPossible +=1
-    return nbPossible
+            nbArrangements += get_nb_possible(patterns,d)
+    return nbPossible,nbArrangements
 
 
-print(get_input(content))
+#print(get_input(content))
 print(solve(content))
+#print(pattern_dict)
